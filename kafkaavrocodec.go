1b03c71fdb228d8bbbe4e3ffc4e6ec6ad9624726
Tim Van Wassenhove <tim@timvw.be>
2019-11-04 16:30:49 +0100

    small refactor

---
+++ kafkaavrocodec.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	schemaregistry "github.com/lensesio/schema-registry"
+	"github.com/linkedin/goavro"
+)
+
+// KafkaAvroCodec decodes kafka avro messages using a schema registry
+type KafkaAvroCodec struct {
+	schemaRegistryClient *cachedSchemaRegistryClient
+	codecCache           map[subjectVersionID]*goavro.Codec
+}
+
+// NewAvroCodec returns a new instance of avrocodec
+func NewAvroCodec(schemaRegistryClient *schemaregistry.Client) (*KafkaAvroCodec) {
+	cachedSchemaRegistryClient := newCachedSchemaRegistryClient(schemaRegistryClient)
+	return &KafkaAvroCodec{cachedSchemaRegistryClient, make(map[subjectVersionID]*goavro.Codec)}
+}
+
+// DecodeValue decodes the value from the given kafka message into a go map
+func (c *KafkaAvroCodec) DecodeValue(msg *kafka.Message) (native interface{}, newBuf []byte, err error) {
+	return c.decode(*msg.TopicPartition.Topic, false, msg.Value)
+}
+
+// DecodeKey decodes the key from the given message into a go map
+func (c *KafkaAvroCodec) DecodeKey(msg *kafka.Message) (native interface{}, newBuf []byte, err error) {
+	return c.decode(*msg.TopicPartition.Topic, true, msg.Key)
+}
+
+// subjectVersionID represents a tuple of a subject and a specific version
+type subjectVersionID struct {
+	subject   string
+	versionID int
+}
+
+func (c *KafkaAvroCodec) decode(topic string, isKey bool, data []byte) (native interface{}, newBuf []byte, err error) {
+
+	subjectVersion, err := extractSubjectAndVersionFromData(topic, isKey, data)
+	if err != nil {
+		return
+	}
+
+	codec, err := c.getCodecFor(subjectVersion)
+	if err != nil {
+		return
+	}
+
+	return codec.NativeFromBinary(data[5:])
+}
+
+func extractSubjectAndVersionFromData(topic string, isKey bool, data []byte) (key subjectVersionID, err error) {
+
+	magicByte := data[0]
+
+	if magicByte != 0 {
+		err = errors.New("Unknown magic byte")
+		return
+	}
+
+	subject := getSubject(topic, isKey)
+	versionID := getSchemaID(data[1:5])
+	key = subjectVersionID{subject, versionID}
+	return
+}
+
+func getSubject(topic string, isKey bool) (subject string) {
+	if isKey {
+		return fmt.Sprintf("%v-key", topic)
+	}
+
+	return fmt.Sprintf("%v-value", topic)
+}
+
+func getSchemaID(data []byte) int {
+	return int(binary.BigEndian.Uint32(data))
+}
+
+func (c *KafkaAvroCodec) getCodecFor(subjectVersion subjectVersionID) (codec *goavro.Codec, err error) {
+
+	codec, ok := c.codecCache[subjectVersion]
+
+	if !ok {
+		var schema string
+		schema, err = c.schemaRegistryClient.getSchemaFor(subjectVersion)
+		if err != nil {
+			return
+		}
+		codec, err = goavro.NewCodec(schema)
+		if err != nil {
+			return
+		}
+		c.codecCache[subjectVersion] = codec
+	}
+
+	return
+}
+
+type cachedSchemaRegistryClient struct {
+	client *schemaregistry.Client
+	cache  map[subjectVersionID]string
+}
+
+func newCachedSchemaRegistryClient(client *schemaregistry.Client) (instance *cachedSchemaRegistryClient) {
+	return &cachedSchemaRegistryClient{client, make(map[subjectVersionID]string)}
+}
+
+func (c *cachedSchemaRegistryClient) getSchemaFor(x subjectVersionID) (schema string, err error) {
+
+	schema, ok := c.cache[x]
+
+	if !ok {
+		var avroSchema schemaregistry.Schema
+		avroSchema, err = c.client.GetSchemaBySubject(x.subject, x.versionID)
+		if err != nil {
+			return
+		}
+		schema = avroSchema.Schema
+		c.cache[x] = schema
+	}
+
+	return
+}
+
